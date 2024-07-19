b0615d3a9101ed21b25253e77a559646aa4d146a
EagleSandrik <eaglesandrik@gmail.com>
2024-07-19 03:41:33 -0400

    Create Meme.py
    Meme

---
+++ Meme.py
@@ -0,0 +1,14 @@
+meme_dict = {"lol": "Very funny (Laughting out loud)",
+"sigma": "Very cool person, does something respectfully",
+"ohio": "State in US, Became famous after a meme",
+"cringe": "Funny, Weird",
+"ohio final boss": "Main character",
+"gigachad": "Has a long chin",
+"mewwing": "Something that is making your chin bigger, Not permanenet"} 
+
+
+
+
+word = input("").lower()
+if word in meme_dict.keys():
+    print(meme_dict[word])
